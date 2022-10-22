44058eedda5b539f14407ba2013346c6c84cf021
alfredjynx <99195331+alfredjynx@users.noreply.github.com>
2022-10-21 22:04:18 -0300

    progress no lugar correto

--- general_league/reward_functions/reward-mat-2.py
+++ general_league/reward_functions/reward-mat-2.py
@@ -1,13 +1,12 @@
-
-# - Model name: Hello-World-De-Carrinho-Versao-Matematica-Avancada
-# - Date: 10/21/2022, 9:51:12 PM GMT-3
+# - Model name: Hello-World-De-Carrinho-Versao-Matematica-Avancada-2
+# - Date: 10/21/2022, 10:02:31 PM GMT-3
 # - Training time: 2h
 # - Technical features: 
 #     track alignment,
 #     more than half-speed, 
 #     limited steering angle, 
 #     drive near to track center
-# - Track: 2022 re:Invent Championship
+# - Track: Jennens Family Speedway
 # - Race type: Time trial
 # - Training algorithm: PPO
 # - Hyperparameters: standard
@@ -21,22 +20,20 @@ def reward_function(params):
     # Example of rewarding the agent to stay inside the two borders of the track
     
     # Read input parameters
-    all_wheels_on_track = params['all_wheels_on_track']
     distance_from_center = params['distance_from_center']
     steering_angle = params['steering_angle']
     speed = params['speed']
     is_offtrack = params["is_offtrack"]
     crashed = params["is_crashed"]
-    
-    
-    ABS_STEERING_THRESHOLD = 20
+    progress = params['progress']
     
     r = speed
     k = distance_from_center
     n = steering_angle
+    m = progress
     
     # reward = r*((1e-3)**((-k)*n)) --------> antiga
-    reward = r*((n)**(-k))
+    reward = progress*r*((n)**(-k))
     
     if not is_offtrack and not crashed:
         reward+=1
