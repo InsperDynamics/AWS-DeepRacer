ceae0747add1257dafda01521fe9092b18adfcdd
alfredjynx <99195331+alfredjynx@users.noreply.github.com>
2022-10-21 21:56:32 -0300

    mudança na equação de reward

--- general_league/reward_functions/reward-mat.py
+++ general_league/reward_functions/reward-mat.py
@@ -1,3 +1,22 @@
+
+# - Model name: Hello-World-De-Carrinho-Versao-Matematica-Avancada
+# - Date: 10/21/2022, 9:51:26 PM GMT-3
+# - Training time: 2h
+# - Technical features: 
+#     track alignment,
+#     more than half-speed, 
+#     limited steering angle, 
+#     drive near to track center
+# - Track: 2022 re:Invent Championship
+# - Race type: Time trial
+# - Training algorithm: PPO
+# - Hyperparameters: standard
+# - Space set: continuous
+# - Speed: range from 0.5 to 4.0
+# - Steering angle: range from -20 to 20
+# - Best time: 
+
+
 def reward_function(params):
     # Example of rewarding the agent to stay inside the two borders of the track
     
@@ -16,7 +35,8 @@ def reward_function(params):
     k = distance_from_center
     n = steering_angle
     
-    reward = r*((1e-3)**((-k)*n))
+    # reward = r*((1e-3)**((-k)*n)) --------> antiga
+    reward = r*((n)**(-k))
     
     if not is_offtrack and not crashed:
         reward+=1
